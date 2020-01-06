47cb0f8897ae71fadabdf83d43b38eb9cf3515cf
Tatum Daily <tgd9019@outlook.com>
2020-01-05 20:14:43 -0600

    no errors but still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,45 @@
 // Game Variables //
+var wins = 0;
+var losses = 0;
+var attempts = 10;
+var gameWord;
+// window.addEventListener("keydown", userGuess)
+var secretLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                    "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                    "w", "x", "y", "z"]
+var wrongGuess = [];
 
-    var wins = 0;
-    var losses = 0;
-    var attempts = 10;
-    var gameWord;
-    var guess = 
-    
-    window.addEventListener("keydown", userGuess)
-    
-// Game Arrays //
 
-    var secretLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-                        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-                        "w", "x", "y", "z"]
-    var wrongGuess = [];
-
+document.getElementById("wins").innerHTML = "Wins: " + wins
+document.getElementById("losses").innerHTML = "Losses: " + losses
+document.getElementById("attempts").innerHTML = "Guesses Remaining: " + attempts
+document.getElementById("wrong").innerHTML = "Letters Guessed so far: " + wrongGuess
 
 // Game Functions //    
 
-    var gameWord = secretLetter[Math.floor(Math.random() * secretLetter.length)]
-    console.log(gameWord)
+var gameWord = secretLetter[Math.floor(Math.random() * secretLetter.length)]
+console.log(gameWord)
 
-    function userGuess() {
-        console.log("you push key")
-    }
-
-    function psychic() {
-        document.getElementById("wins").innerHTML = "Wins: " + wins
-        document.getElementById("losses").innerHTML = "Losses: " + losses
-        document.getElementById("attempts").innerHTML = "Guesses Remaining: " + attempts
-        document.getElementById("wrong").innerHTML = "Letters Guessed so far: " + wrongGuess
-
-        if (gameWord === guess) {
-            wins++
-        } else if (guess !== gameWord) {
-            attempts--
-            wrongGuess.push(guess)
-        } 
-        
-        if (attempts === 0) {
-            losses++
-        }
-        
-     }
-
-// Game Loops //
-
+document.onkeydown = function(e) {
+    guess = e.key        
+    console.log(guess)
     
-    psychic();
+    if (guess === gameWord) {
+        wins++
+        reset()
+    } else if (guess !== gameWord) {
+        attempts--
+        wrongGuess.push(guess)
+    } else if (attempts === 0) {
+        losses++
+        reset()
+    }
+}    
+
+function reset() {
+    document.getElementById("wins").innerHTML = "Wins: " + wins
+    document.getElementById("losses").innerHTML = "Losses: " + losses
+    document.getElementById("attempts").innerHTML = "Guesses Remaining: " + attempts
+    document.getElementById("wrong").innerHTML = "Letters Guessed so far: " + wrongGuess
+}
+
